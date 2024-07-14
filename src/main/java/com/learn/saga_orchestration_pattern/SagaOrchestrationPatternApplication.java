7e0d0dd31399ff8f4b5610d97d0843cc69e42fb2
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-07-14 18:04:32 +0530

    first

---
+++ src/main/java/com/learn/saga_orchestration_pattern/SagaOrchestrationPatternApplication.java
@@ -0,0 +1,13 @@
+package com.learn.saga_orchestration_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaOrchestrationPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaOrchestrationPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/saga_orchestration_pattern/SagaOrchestrationPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.saga_orchestration_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaOrchestrationPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
